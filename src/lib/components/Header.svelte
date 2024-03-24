dba9eb46872b35fe1d23b18f1cf3d5f1c3fda718
Dayo <dayonel@hotmail.es>
2024-03-24 02:32:00 +0100

    Add fabric js.

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,23 +6,31 @@
 </script>
 
 <header>
-  <Logo></Logo>
+  <div class="header-container">
+    <Logo></Logo>
 
-  <nav class="flex items-center justify-between space-x-2">
-    <Button on:click={toggleMode} variant="outline" size="icon">
-      <Sun
-        class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-      />
-      <Moon
-        class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-      />
-      <span class="sr-only">Toggle theme</span>
-    </Button>
-  </nav>
+    <nav class="flex items-center justify-between space-x-2">
+      <Button on:click={toggleMode} variant="outline" size="icon">
+        <Sun
+          class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        />
+        <Moon
+          class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        />
+        <span class="sr-only">Toggle theme</span>
+      </Button>
+    </nav>
+  </div>
 </header>
 
 <style>
   header {
-    @apply flex justify-between items-center w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 h-16 container max-w-screen-2xl;
+    @apply fixed z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 h-16;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .header-container {
+    @apply container flex justify-between items-center;
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { fabric } from "fabric";
+  import { onMount } from "svelte";
+
+  let canvas: HTMLCanvasElement;
+  let fb: fabric.Canvas;
+  onMount(() => {
+    fb = new fabric.Canvas(canvas, {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+
+    const rect = new fabric.Rect({
+      left: 150,
+      top: 150,
+      width: 20,
+      height: 15,
+      fill: "blue",
+    });
+    fb.add(rect);
+  });
+
+  const resize = () => {
+    fb.setWidth(window.innerWidth);
+    fb.setHeight(window.innerHeight);
+  };
+</script>
+
+<svelte:window on:resize={resize} />
+
+<canvas bind:this={canvas} id="canvas" width="300" height="300"></canvas>
